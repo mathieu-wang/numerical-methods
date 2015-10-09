889b709873dba5ca3e4e79cb9e73996a9fe70d0a
Mathieu Wang <mathieu.sy.wang@gmail.com>
2015-10-08 22:24:54 -0700

    Add back substitution

--- cholesky.py
+++ cholesky.py
@@ -7,22 +7,31 @@ from util import mult
 
 
 def decompose(A):
-    L = [[0.0] * len(A) for _ in xrange(len(A))]
+    L = [[0.0] * len(A) for _ in xrange(len(A))] # create L with same size as A
     for i in xrange(len(A)):
         for j in xrange(i+1):
             s = sum(L[i][k] * L[j][k] for k in xrange(j))
             L[i][j] = sqrt(A[i][i] - s) if (i == j) else (1.0 / L[j][j] * (A[i][j] - s))
     return L
 
+
 def forward_elim(L, b):
     for j in xrange(len(L)):
         b[j] = b[j]/L[j][j]
-        for i in range(j+1, len(L)):
+        for i in range(j+1, len(L)+1):
             b[i] = b[i] - L[i][j]*b[j] # look ahead
-    return b
+    return b #actually y
 
-def back_substitution():
-    pass
+
+def back_substitution(L, y):
+    x = [[0.0] * len(y) for _ in xrange(len(y))] # create x with same size as y
+    for i in range(len(L)-1, -1, -1):
+        sum = 0
+        for j in range(i+1, len(L)):
+            sum += L[j][i] * x[j]
+        x[i] = (y[i] - sum) * 1.0 / L[i][i]
+    return x
+
 
 if __name__ == "__main__":
     A1 = [[25, 15, -5],
