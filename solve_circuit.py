d0dfad6b18b1657684050984be3650bf2da4634c
Mathieu Wang <mathieu.sy.wang@gmail.com>
2015-10-15 23:10:34 -0400

    Fix incidence matrix and setup matrix equation

--- solve_circuit.py
+++ solve_circuit.py
@@ -1,6 +1,9 @@
 from cholesky import solve
 from util import vector_to_diag_matrix
+from util import transpose
+from util import mult
 from pprint import pprint
+
 
 def read_circuit_from_file(filename):
 
@@ -9,10 +12,18 @@ def read_circuit_from_file(filename):
 
     num_branches = int(lines[0])
     J = [[float(line.split(' ')[0])] for line in lines[1:1+num_branches]]
-    y = [[float(line.split(' ')[1])] for line in lines[1:1+num_branches]]
-    vector_to_diag_matrix(y)
+    y = [[1/float(line.split(' ')[1])] for line in lines[1:1+num_branches]]
+    vector_to_diag_matrix(y) # pad with zeros to produce diagonal matrix
     E = [[float(line.split(' ')[2])] for line in lines[1:1+num_branches]]
     A = [[float(val) for val in line.split()] for line in lines[1+num_branches:]]
+
+    A_transpose = transpose(A)
+
+    AyAt = mult(mult(A, y), A_transpose)
+    b = mult(A, J) #TODO add -JE
+
+    pprint(solve(AyAt, b))
+
     pprint(J)
     pprint(E)
     pprint(A)
