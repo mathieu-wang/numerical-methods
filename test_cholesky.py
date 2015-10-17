293b659b3a7d6e389c8b826873ebeeccfcffe9c6
Mathieu Wang <mathieu.sy.wang@gmail.com>
2015-10-17 18:18:13 -0400

    Add util methods and test cases

--- test_cholesky.py
+++ test_cholesky.py
@@ -2,20 +2,47 @@ import random
 from util import transpose
 from util import vector_to_diag_matrix
 from util import mult
-from pprint import pprint
+from util import det
+from util import equal
+from util import print_mat
+import unittest
+
 
 def generate_sym_pos_def_matrix(n, max):
-    random_pos_matrix = [[float('%.2f'%(random.random()*max)) for i in range(n)] for i in range(n)]
+    random_pos_matrix = [[float('%.2f' % (random.random() * max)) for i in range(n)] for i in range(n)]
     random_pos_matrix_transpose = transpose(random_pos_matrix)
-    random_diagonal_matrix_vector = [[float('%.2f'%(random.random()*max))] for i in range(n)]
+    random_diagonal_matrix_vector = [[float('%.2f' % (random.random() * max))] for i in range(n)]
     random_diagonal_matrix = vector_to_diag_matrix(random_diagonal_matrix_vector)
     result = mult(mult(random_pos_matrix, random_diagonal_matrix), random_pos_matrix_transpose)
 
-    pprint(random_pos_matrix)
-    pprint(random_pos_matrix_transpose)
-    pprint(random_diagonal_matrix_vector)
-    pprint(random_diagonal_matrix)
-    pprint(result)
+    # pprint(random_pos_matrix)
+    # pprint(random_pos_matrix_transpose)
+    # pprint(random_diagonal_matrix_vector)
+    # pprint(random_diagonal_matrix)
+    # pprint(result)
+    return result
+
+
+class TestCholesky(unittest.TestCase):
+    def setUp(self):
+        print "In method", self._testMethodName
+
+    def test_generate_sym_pos_def_matrix(self):
+        matrix = generate_sym_pos_def_matrix(5, 5)
+        print("matrix:")
+        print_mat(matrix)
+        print("det(matrix):")
+        print(det(matrix))
+        self.assertTrue(equal(matrix, transpose(matrix)))
+        self.assertTrue(det(matrix) > 0)
+
+    def test_cholesky_2x2(self):
+        matrix = generate_sym_pos_def_matrix(2, 10)
+        print("A:")
+        print_mat(matrix)
+        print("det(matrix):")
+        print(det(matrix))
+
 
 if __name__ == '__main__':
-    matrix = generate_sym_pos_def_matrix(3, 10)
+    unittest.main()
--- util.py
+++ util.py
@@ -1,13 +1,18 @@
+from prettytable import PrettyTable
+
+
 def transpose(A):
     return [list(x) for x in zip(*A)]
+
 
 def mult(A, B):
     result = [[0 for x in xrange(len(B[0]))] for x in xrange(len(A))]
     for i in xrange(len(A)):
-       for j in xrange(len(B[0])):
-           for k in range(len(B)):
-               result[i][j] += A[i][k] * B[k][j]
+        for j in xrange(len(B[0])):
+            for k in range(len(B)):
+                result[i][j] += A[i][k] * B[k][j]
     return result
+
 
 def equal(A, B):
     if len(A) != len(B) or len(A[0]) != len(B[0]):
@@ -15,15 +20,41 @@ def equal(A, B):
     else:
         for i in xrange(len(A)):
             for j in xrange(len(A[0])):
-                if A[i][j] != B[i][j]:
+                if abs(A[i][j] - B[i][j]) > 0.000001:
                     return False
     return True
 
+
 def vector_to_diag_matrix(vector):
     diag = [[e for e in innerlist] for innerlist in vector]
-    for idx, val in enumerate(diag): # Make y diagonal
-        for i in xrange(len(diag)-1-idx):
+    for idx, val in enumerate(diag):  # Make y diagonal
+        for i in xrange(len(diag) - 1 - idx):
             val.append(0)
         for i in xrange(idx):
             val.insert(0, 0)
     return diag
+
+
+# Inspired by http://codegolf.stackexchange.com/questions/8405/matrix-determinant
+def det(x):
+    l = len(x)
+    if l == 1:
+        return x[0][0]
+    return sum([(-1) ** i * x[i][0] * det(minor(x, i + 1, 1)) for i in range(l)])
+
+
+# Inspired by http://codegolf.stackexchange.com/questions/8405/matrix-determinant
+def minor(x, i, j):
+    y = x[:]
+    del (y[i - 1])
+    y = zip(*y)
+    del (y[j - 1])
+    return zip(*y)
+
+
+def print_mat(matrix):
+    p = PrettyTable()
+    for row in matrix:
+        p.add_row(row)
+
+    print p.get_string(header=False, border=False)
