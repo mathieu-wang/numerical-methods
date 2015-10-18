437facd030330e400e5b88af84817ad08ecff2de
Mathieu Wang <mathieu.sy.wang@gmail.com>
2015-10-18 18:52:00 -0400

    Move back to merging last node into first, but fix E matrix

--- linear_resistive_network.py
+++ linear_resistive_network.py
@@ -4,15 +4,16 @@ from solve_circuit import solve_circuit
 
 
 def connect_last_node_to_first(incidence_matrix):
-    for ind, value in enumerate(incidence_matrix[1]):
+    for ind, value in enumerate(incidence_matrix[-1]):
         if value != 0:
-            incidence_matrix[-1][ind] = value
+            incidence_matrix[1][ind] = value
 
 
 def remove_unnecessary_data(incidence_matrix):
     del incidence_matrix[0]
     for row in incidence_matrix:
         del row[0]
+    print_mat(incidence_matrix)
     del incidence_matrix[0]
     del incidence_matrix[-1] # TO GET REDUCED
 
@@ -36,14 +37,14 @@ def generate_circuit_file(N):
     connect_last_node_to_first(incidence_matrix)
     remove_unnecessary_data(incidence_matrix)
 
-    print_mat(incidence_matrix)
+    # print_mat(incidence_matrix)
 
     jre_matrix = [[0 for x in xrange(3)] for x in xrange(num_branches)]
     for i in xrange(num_branches):
         jre_matrix[i][1] = 1000
 
     jre_matrix[0][2] = 10
-    jre_matrix[num_branches / 2 + 1][2] = 10
+    jre_matrix[(N+1)*2*N][2] = 10
 
     filename = "linearResistorNetwork{}x{}.txt".format(N, 2*N)
     with open(filename, 'w') as f:
@@ -64,7 +65,7 @@ def format_matrix_for_file(matrix):
 
 
 if __name__ == '__main__':
-    N = 2
+    N = 
     filename, num_branches = generate_circuit_file(N)
     J, y, E, A = read_circuit_from_file(filename)
     Vn = solve_circuit(J, y, E, A)
