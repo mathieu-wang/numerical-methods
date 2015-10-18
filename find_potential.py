e12ec0ae549b54f99e6ec095eb658820572de673
Mathieu Wang <mathieu.sy.wang@gmail.com>
2015-10-18 14:27:40 -0400

    Simply use of symmetry to update the other 3 quadrants

--- find_potential.py
+++ find_potential.py
@@ -28,22 +28,17 @@ def sor(v_inner, w_inner, h_inner, length_outer, h, omega):
     done = False
     num_iterations = 0
     while not done:
-        done = True # assume all residues are small enough. Change to false if at least one residue is too large
+        done = True  # assume all residues are small enough. Change to false if at least one residue is too large
 
-        # Update lower-left quadrant
         for i in range(1, (size_of_mesh_matrix / 2) + 1):
             for j in range(1, (size_of_mesh_matrix / 2) + 1):
                 if not (inner_x_left_node_index <= i <= inner_x_right_node_index and inner_y_bottom_node_index <= j <= inner_y_top_node_index):
+                    # Lower Left Quadrant:
                     mesh_matrix[i][j] = (1 - omega) * mesh_matrix[i][j] + omega / 4 * (mesh_matrix[i-1][j] + mesh_matrix[i][j-1] + mesh_matrix[i+1][j] + mesh_matrix[i][j+1])
-
-        # Update other quadrants
-        for i in range(1, (size_of_mesh_matrix / 2)):
-            for j in range(1, (size_of_mesh_matrix / 2)):
-                mesh_matrix[i][size_of_mesh_matrix-j-1] = mesh_matrix[i][j]
-                mesh_matrix[size_of_mesh_matrix - i-1][j] = mesh_matrix[i][j]
-                mesh_matrix[size_of_mesh_matrix - i-1][size_of_mesh_matrix-j-1] = mesh_matrix[i][j]
-
-        # print_mat(mesh_matrix)
+                    # Use symmetry to update the other 3 quadrants:
+                    mesh_matrix[i][size_of_mesh_matrix - j - 1] = mesh_matrix[i][j]
+                    mesh_matrix[size_of_mesh_matrix - i - 1][j] = mesh_matrix[i][j]
+                    mesh_matrix[size_of_mesh_matrix - i - 1][size_of_mesh_matrix - j - 1] = mesh_matrix[i][j]
 
         for i in range(1, (size_of_mesh_matrix / 2) + 1):
             for j in range(1, (size_of_mesh_matrix / 2) + 1):
@@ -52,13 +47,10 @@ def sor(v_inner, w_inner, h_inner, length_outer, h, omega):
                     print residue
                     if abs(residue) >= 0.00001:
                         done = False
-                        break
-            if not done:
-                break
         num_iterations += 1
         # print(iterations)
     print_mat(mesh_matrix)
-    print(num_iterations)
+    print "Iterations:", num_iterations
     return mesh_matrix
 
 
@@ -67,6 +59,4 @@ if __name__ == '__main__':
     potential_matrix = sor(15, 0.08, 0.04, 0.2, h, 1.0)
     node_x = int(0.06 / h)
     node_y = int(0.04 / h)
-    print node_x
-    print node_y
     print potential_matrix[node_x][node_y]
